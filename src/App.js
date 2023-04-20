f2986c744cce1e3cf2fee6d74d62a2f2fd4b7ab3
Jeffrey-Z1030 <113541414+Jeffrey-Z1030@users.noreply.github.com>
2023-04-20 03:08:52 -0400

    started navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import AllProducts from "./Components/AllProducts";
+import LandingPage from "./Components/LandingPage";
+import { Routes,Route } from "react-router-dom";
+
 
 function App() {
 	return (
 		<div className="App">
 			<header>
-				<NavBar />
+				<NavBar/>
 			</header>
       <main>
-        <AllProducts/>
+        <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='allProduct' element={<AllProducts/>}/>
+        </Routes>
       </main>
 		</div>
 	);
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,54 @@
-function NavBar(){
-    return(
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { AppBar } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+function NavBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            <Link to='/'>Store</Link>
+          </Typography>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}><Link to='allProduct'>All Items</Link></MenuItem>
+            <MenuItem onClick={handleClose}><Link>Option 2</Link></MenuItem>
+            <MenuItem onClick={handleClose}><Link>Option 3</Link></MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default NavBar
+export default NavBar;
+
